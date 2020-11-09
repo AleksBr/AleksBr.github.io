63d675daa98ae46d7cea77bd955a05c2aeec12cc
AleksandrBr <aleksandr.bondarenko@jelvix.com>
2020-11-09 16:27:41 +0200

    added json

--- js/linux.js
+++ js/linux.js
@@ -1,18 +1,38 @@
 (function ($) {
-    $(document).ready(function() {
+    $(document).ready(function () {
 
-        $.getJSON( "linux-command.json", function( data ) {
+        $.getJSON("linux-command.json", function (data) {
 
             let items = [];
 
-            $.each( data.commands, function( key, val ) {
-                items.push( "<li id='" + key + "'>" + val['name'] + "</li>" );
+            $.each(data, function (key, val) {
+                items.push("<li id='" + key + "'>" + key + "</li>");
             });
 
-            $( "<ul/>", {
-                "class": "my-new-list",
-                html: items.join( "" )
-            }).appendTo( "body" );
+            $("<ul/>", {
+                "class": "js-command-list",
+                html: items.join("")
+            }).appendTo(".list-block");
+        });
+
+        $('.list-block').on('click', 'li', function (e) {
+            $('.info-block').html('');
+            let target = $(this).attr('id');
+            let desc = "";
+            let example = [];
+
+            $.getJSON("linux-command.json", function (data) {
+
+                $.each(data, function (key, val) {
+                    if(key === target) {
+                        desc = val['description'];
+                        example = val['example'];
+                    }
+                });
+
+                $('.info-block').append(desc);
+                $('.info-block').append(example);
+            });
         });
 
     });
