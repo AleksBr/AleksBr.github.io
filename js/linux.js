0b8a018eeabe90c356fd4c6b3f1e3993196c2137
AleksBr <ss.aleks.b@gmail.com>
2020-11-10 00:38:27 +0200

    added foundation css

--- js/linux.js
+++ js/linux.js
@@ -6,17 +6,21 @@
             let items = [];
 
             $.each(data, function (key, val) {
-                items.push("<li id='" + key + "'>" + key + "</li>");
+                items.push("<a href='#' class='button' id='" + key + "'>" + key + "</a>");
             });
 
-            $("<ul/>", {
-                "class": "js-command-list",
-                html: items.join("")
-            }).appendTo(".list-block");
+            // $("<ul/>", {
+            //     "class": "js-command-list",
+            //     html: items.join("")
+            // }).appendTo(".list-block");
+
+            $('.list-block').append(items);
         });
 
-        $('.list-block').on('click', 'li', function (e) {
+        $('.list-block').on('click', 'a', function (e) {
+            e.preventDefault();
             $('.info-block').html('');
+            $('.example-block').html('');
             let target = $(this).attr('id');
             let desc = "";
             let example = [];
@@ -24,14 +28,18 @@
             $.getJSON("linux-command.json", function (data) {
 
                 $.each(data, function (key, val) {
-                    if(key === target) {
+                    if (key === target) {
                         desc = val['description'];
                         example = val['example'];
+                        return false;
+                    } else {
+                        desc = "не заполнено";
+                        example = "";
                     }
                 });
 
                 $('.info-block').append(desc);
-                $('.info-block').append(example);
+                $('.example-block').append(example);
             });
         });
 
